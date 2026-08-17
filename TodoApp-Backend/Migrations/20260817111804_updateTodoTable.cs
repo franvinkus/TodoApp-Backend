954ac64074314225e75285156cbe66e63a2c108d
franvinkus <vincentkurnia29@gmail.com>
2026-08-17 18:19:16 +0700

    feat: -Update Todo table

---
+++ TodoApp-Backend/Migrations/20260817111804_updateTodoTable.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TodoApp_Backend.Migrations
+{
+    /// <inheritdoc />
+    public partial class updateTodoTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "EndDate",
+                table: "Todos",
+                type: "timestamp with time zone",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "StartDate",
+                table: "Todos",
+                type: "timestamp with time zone",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "EndDate",
+                table: "Todos");
+
+            migrationBuilder.DropColumn(
+                name: "StartDate",
+                table: "Todos");
+        }
+    }
+}
--- TodoApp-Backend/Models/Todo.cs
+++ TodoApp-Backend/Models/Todo.cs
@@ -9,13 +9,11 @@ public partial class Todo
     public Guid UserId { get; set; }
     public Users User { get; set; }
     public string Title { get; set; } = null!;
-
     public string? Description { get; set; }
-
+    public DateTime StartDate { get; set; }
+    public DateTime EndDate { get; set; }
     public DateTime CreatedDate { get; set; }
-
     public DateTime? FinishedDate { get; set; }
-
     public bool IsFinished { get; set; }
 
 }
