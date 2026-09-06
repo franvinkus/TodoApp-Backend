59efb668d44e553393a7794362b0d12bc614ed2e
franvinkus <vincentkurnia29@gmail.com>
2026-09-07 06:10:59 +0700

    feat: Changed email service from smtp to resend

--- TodoApp-Backend/Services/EmailServices.cs
+++ TodoApp-Backend/Services/EmailServices.cs
@@ -3,6 +3,7 @@ using MailKit.Net.Smtp;
 using MailKit.Security;
 using Microsoft.Extensions.Configuration;
 using MimeKit;
+using Resend;
 
 namespace TodoApp_Backend.Services
 {
@@ -15,37 +16,61 @@ namespace TodoApp_Backend.Services
             _config = config;
         }
 
-        public async Task SendEmail(string toEmail,  string subject, string body)
+        public async Task SendEmail(string toEmail, string subject, string body)
         {
-            var email = new MimeMessage();
+            IResend resend = ResendClient.Create("re_2MkwVHqt_D3wHmAJKwfmnhxMcnLY7Ckv7");
 
-            email.From.Add(new MailboxAddress("Vincent's Todo Tasks", _config["EmailSettings:SenderEmail"]));
-            email.To.Add(MailboxAddress.Parse(toEmail));
-            email.Subject = subject;
+            var message = new EmailMessage
+            {
+                From = "noreply@todo.vincentkurnia.com",
+                Subject = subject,
+                HtmlBody = body
+            };
 
-            var builder = new BodyBuilder { HtmlBody = body };
-            email.Body = builder.ToMessageBody();
+            message.To.Add(toEmail);
 
-            using var smtp = new SmtpClient();
             try
             {
-                smtp.Timeout = 120000;
-                await smtp.ConnectAsync(_config["EmailSettings:SmtpHost"], int.Parse(_config["EmailSettings:SmtpPort"]), SecureSocketOptions.StartTls);
-                smtp.AuthenticationMechanisms.Remove("XOAUTH2");
-                await smtp.AuthenticateAsync(_config["EmailSettings:SenderEmail"], _config["EmailSettings:SenderPassword"]);
-
-
-                await smtp.SendAsync(email);
+                await resend.EmailSendAsync(message);
             }
             catch (Exception ex)
             {
-                System.Diagnostics.Debug.WriteLine("Error: ", ex);
-                throw;
-            }
-            finally
-            {
-                await smtp.DisconnectAsync(true);
+                System.Diagnostics.Debug.WriteLine("Error pengiriman API: ", ex);
+                throw; // Lempar kembali agar Hangfire bisa mencatat kegagalan jika API down
             }
         }
+
+        //public async Task SendEmail(string toEmail,  string subject, string body)
+        //{
+        //    var email = new MimeMessage();
+
+        //    email.From.Add(new MailboxAddress("Vincent's Todo Tasks", _config["EmailSettings:SenderEmail"]));
+        //    email.To.Add(MailboxAddress.Parse(toEmail));
+        //    email.Subject = subject;
+
+        //    var builder = new BodyBuilder { HtmlBody = body };
+        //    email.Body = builder.ToMessageBody();
+
+        //    using var smtp = new SmtpClient();
+        //    try
+        //    {
+        //        smtp.Timeout = 120000;
+        //        await smtp.ConnectAsync(_config["EmailSettings:SmtpHost"], int.Parse(_config["EmailSettings:SmtpPort"]), SecureSocketOptions.StartTls);
+        //        smtp.AuthenticationMechanisms.Remove("XOAUTH2");
+        //        await smtp.AuthenticateAsync(_config["EmailSettings:SenderEmail"], _config["EmailSettings:SenderPassword"]);
+
+
+        //        await smtp.SendAsync(email);
+        //    }
+        //    catch (Exception ex)
+        //    {
+        //        System.Diagnostics.Debug.WriteLine("Error: ", ex);
+        //        throw;
+        //    }
+        //    finally
+        //    {
+        //        await smtp.DisconnectAsync(true);
+        //    }
+        //}
     }
 }
